eb295de4f12311dc321ce4bcacddae95600425b2
Dmitry Korzunin <kordimsan@mail.ru>
2021-03-15 23:15:55 +0500

    handle location to buttons

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import telebot
-
+from telebot import types
+import overpy
+import geocoder
+api = overpy.Overpass()
 
 TOKEN = '1648079718:AAHgjkA0pVWro3tlObdSJ3yzjSOja3Yde7I'
 
@@ -10,15 +13,33 @@ def start_command(message):
     bot.send_message(message.chat.id, 'Привет, ты запустил бот который умеет сохранять твои точки интереса!')
 
 @bot.message_handler(commands=['add'])
-def add_command(message):
-    bot.send_message(message.chat.id, 'Ты запустил комманду add – добавление нового места!')
+def handle_add_command(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
+    keyboard.add(button_geo)
+    bot.send_message(message.chat.id, "Нажми на кнопку и передай мне свое местоположение", reply_markup=keyboard)
+
+@bot.message_handler(content_types=["location"])
+def handle_location(message):
+    if message.location is not None:
+        print(message.location)
+        print("latitude: %s; longitude: %s" % (message.location.latitude, message.location.longitude))
+        r = api.query('[out:json];node[~"^(amenity|shop)$"~"."](around:200, {},{}); out;'.format(message.location.latitude, message.location.longitude))
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        for n in r.nodes:
+            g = geocoder.osm([n.lat, n.lon], method='reverse')
+            if n.tags.get('name'):
+                button = types.KeyboardButton(text=n.tags.get('name') + '('+ g.address +')')
+                keyboard.add(button)
+                print(n.tags, n.lat, n.lon)
+        bot.send_message(message.chat.id, 'Найдены точки интереса по близости:', reply_markup=keyboard)
 
 @bot.message_handler(commands=['list'])
-def list_command(message):
+def handle_list_command(message):
     bot.send_message(message.chat.id, 'Ты запустил комманду list – отображение добавленных мест!')
 
 @bot.message_handler(commands=['reset'])
-def reset_command(message):
+def handle_reset_command(message):
     bot.send_message(message.chat.id, 'Ты запустил комманду reset – удалить все его добавленные локации!')
 
 bot.polling()
